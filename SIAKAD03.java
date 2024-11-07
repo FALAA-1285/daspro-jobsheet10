a2e2a9a8e90c54e6ad72dcdbac0e22792c5f6816
FALAA-1285 <ahmadfalahi1285@gmail.com>
2024-11-07 11:31:05 +0700

    pertanyaan

--- SIAKAD03.java
+++ SIAKAD03.java
@@ -5,30 +5,35 @@ public class SIAKAD03 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
-        int[][] nilai=new int [4][3];
+        System.out.print("masukkan jumlah mahasiswa: ");
+        int p=sc.nextInt();
+        System.out.print("masukkan jumlah mata kuliah: ");
+        int h=sc.nextInt();
+
+        int[][] nilai=new int [p][h];
 
         for(int i=0; i<nilai.length; i++){
             System.out.println("masukkan nilai mahasiswa ke-"+(i+1));
             double totalPerSiswa=0;
-            
+
             for (int j=0; j<nilai[i].length; j++) {
                 System.out.print("Mata kuliah ke"+(j+1)+" :");
                 nilai[i][j]=sc.nextInt();
                 totalPerSiswa+=nilai[i][j];
             }
-            System.out.println("Nilai rat-rata: "+totalPerSiswa/3);
+            System.out.println("Nilai rat-rata: "+totalPerSiswa/nilai[i].length);
         }
 
         System.out.println("\n===============================");
         System.out.println("Rata-rata nilai setiap mata kuliah: ");
 
-        for(int j=0; j<3; j++){
+        for(int j=0; j<nilai[0].length; j++){
             double totalPermatkul=0;
 
-            for(int i=0; i<4; i++){
+            for(int i=0; i<nilai.length; i++){
                 totalPermatkul+=nilai[i][j];
             }
-            System.out.println("mata kuliah "+(j+1)+" : "+totalPermatkul/4);
+            System.out.println("mata kuliah "+(j+1)+" : "+totalPermatkul/nilai.length);
         }
     }
     
