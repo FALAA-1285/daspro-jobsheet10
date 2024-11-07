13e11c9e41cb480c2b3b323145f6598d624ecee5
FALAA-1285 <ahmadfalahi1285@gmail.com>
2024-11-07 09:16:29 +0700

    perbaikan percobaan2

--- BioskopWithScanner03.java
+++ BioskopWithScanner03.java
@@ -5,21 +5,23 @@ public class BioskopWithScanner03 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
+        int baris, kolom;
+        String nama, next;
         String[][] penonton=new String[4][2];
 
         while(true){
             System.out.print("Masukkan nama: ");
-            String nama=sc.nextLine();
+            nama=sc.nextLine();
             System.out.print("Masukkan baris: ");
-            int baris=sc.nextInt();
+            baris=sc.nextInt();
             System.out.print("Masukkan kolom: ");
-            int kolom=sc.nextInt();
+            kolom=sc.nextInt();
             sc.nextLine();
 
             penonton[baris-1][kolom-1]=nama;
 
             System.out.print("input penonton isinya? (y/n):");
-            String next=sc.nextLine();
+            next=sc.nextLine();
 
             if(next.equalsIgnoreCase("n")){
                 break;
