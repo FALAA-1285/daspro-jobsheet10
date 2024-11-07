fdd35c6a72a6a83a39293ccf13e9424219eccb5f
FALAA-1285 <ahmadfalahi1285@gmail.com>
2024-11-07 11:17:32 +0700

    percobaan4

--- SIAKAD03.java
+++ SIAKAD03.java
@@ -7,16 +7,18 @@ public class SIAKAD03 {
 
         int[][] nilai=new int [4][3];
 
-        for(int i=0; i<4; i++){
+        for(int i=0; i<nilai.length; i++){
             System.out.println("masukkan nilai mahasiswa ke-"+(i+1));
             double totalPerSiswa=0;
-            for (int j=0; j<3; j++) {
+            
+            for (int j=0; j<nilai[i].length; j++) {
                 System.out.print("Mata kuliah ke"+(j+1)+" :");
                 nilai[i][j]=sc.nextInt();
                 totalPerSiswa+=nilai[i][j];
             }
             System.out.println("Nilai rat-rata: "+totalPerSiswa/3);
         }
+
         System.out.println("\n===============================");
         System.out.println("Rata-rata nilai setiap mata kuliah: ");
 
